6c3c558acfde6fd45de9340e22e8123a379da1ab
qiao <qiao@qiao-PC>
2017-03-14 16:14:54 +0800

    swagger fix

--- src/main/java/com/qiao/Application.java
+++ src/main/java/com/qiao/Application.java
@@ -2,12 +2,10 @@ package com.qiao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
-@EnableCaching
+//@EnableAsync
+//@EnableCaching
 public class Application {
 	
 	public static void main(String[] args) {
